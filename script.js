0a853bbf84e0c259d0e6adbf36612572924f4031
mariajoaosantos <mariajcs18@gmail.com>
2022-08-15 14:57:58 +0100

    Styling done - missing script

---
+++ script.js
@@ -0,0 +1,10 @@
+const vacation = '1 Oct 2022'
+
+function countdown() = {
+  const vacationDate = new Date(vacationDate);
+  const currentDate = new Date();
+
+  console.log(vacationDate - currentDate)
+}
+
+countdown();
